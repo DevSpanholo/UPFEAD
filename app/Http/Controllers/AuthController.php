29b6086c64d09c9830f23efafefe9ef9900ff951
DevSpanholo <devspanholo@gmail.com>
2023-10-23 23:00:57 -0300

    Revert commit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
 
 use Validator;
 use Hash;
 use Auth;
 use App\Models\User;
+
 
 class AuthController extends Controller
 {
@@ -19,59 +23,61 @@ class AuthController extends Controller
     }
   
     public function registerSave(Request $request)
-{
-    Validator::make($request->all(), [
-        'username' => 'required',
-        'email' => 'required|email|unique:users,email',
-        'password' => 'required|confirmed'
-    ])->validate();
-
-    User::create([
-        'username' => $request->username,
-        'email' => $request->email,
-        'password' => Hash::make($request->password),
-        'role' => 'Aluno'
-    ]);
-        
-
-    return redirect()->route('login');
-}
-
-  
-    public function login()
-    {
-        return view('auth/login');
-    }
-  
-    public function loginAction(Request $request)
     {
         Validator::make($request->all(), [
-            'email' => 'required|email',
-            'password' => 'required'
+            'username' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|confirmed'
         ])->validate();
-  
-        if (!Auth::attempt($request->only('email', 'password'), $request->boolean('remember'))) {
-            throw ValidationException::withMessages([
-                'email' => trans('auth.failed')
-            ]);
+
+        User::create([
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => 'Aluno'
+        ]);
+        
+        return redirect()->route('login');
+    }
+
+    public function login(Request $request)
+{
+    $credentials = $request->only('email', 'password');
+    
+    try {
+        $token = JWTAuth::attempt($credentials);
+        
+        if (!$token) {
+            return back()->with('error', 'Credenciais inválidas. Por favor, tente novamente.');
         }
-  
-        $request->session()->regenerate();
-  
-        return redirect()->route('dashboard');
+
+        $request->session()->put('jwt_token', $token);
+        dd($token);
+
+        if (!auth()->check()) {
+            return back()->with('error', 'Erro na autenticação. Por favor, tente novamente.');
+        }
+
+    } catch (\Exception $e) {
+        return back()->with('error', 'Erro ao fazer login: ' . $e->getMessage());
     }
-  
-    public function logout(Request $request)
+    
+    return redirect()->route('dashboard');
+}
+    
+    public function logout()
     {
-        Auth::guard('web')->logout();
-  
-        $request->session()->invalidate();
-  
-        return redirect('/');
+        auth()->logout();
+        return response()->json(['message' => 'Successfully logged out']);
     }
- 
+
     public function profile()
     {
         return view('profile');
+    }
+
+    public function showLoginForm()
+    {
+        return view('auth.login');
     }
 }
