5930b34dc1d72a613a35550ddcc5225e57bff9ea
DevSpanholo <devspanholo@gmail.com>
2023-10-26 02:51:54 -0300

    Ajustes

--- resources/js/Component/FormSlide.vue
+++ resources/js/Component/FormSlide.vue
@@ -1,58 +1,58 @@
 <template>
     <div>
-      <vc-slide :add-slide="testeFn" />
+        <vc-slide :add-slide="testeFn" />
 
-      <div>
-        <Carousel :items-to-show="1" :wrap-around="true">
-            <Slide v-for="(slide, index) in list" :key="index">
-                <div class="slide-preview">
-                  <div class="slide-content">
-                    <h3>{{ slide.title }}</h3>
-                    <div v-html="slide.description"></div>
-                  </div>
-                </div>
-              </Slide>
+        <div>
+            <Carousel :items-to-show="1" :wrap-around="true">
+                <Slide v-for="(slide, index) in list" :key="index">
+                    <div class="slide-preview">
+                        <div class="slide-content">
+                            <h3>{{ slide.title }}</h3>
+                            <div v-html="slide.description"></div>
+                        </div>
+                    </div>
+                </Slide>
 
-          <template #addons>
-            <Navigation />
-          </template>
-        </Carousel>
-      </div>
-
-      <div class="row" style="display: none;">
-        <div v-for="(slide, index) in list" :key="index">
-          <input type="hidden" :name="'slides[' + index + '][title]'" v-model="slide.title" />
-          <input type="hidden" :name="'slides[' + index + '][description]'" v-model="slide.description" />
+                <template #addons>
+                    <Navigation />
+                </template>
+            </Carousel>
         </div>
-      </div>
+
+        <div class="row" style="display: none;">
+            <div v-for="(slide, index) in list" :key="index">
+                <input type="hidden" :name="'slides[' + index + '][title]'" v-model="slide.title" />
+                <input type="hidden" :name="'slides[' + index + '][description]'" v-model="slide.description" />
+            </div>
+        </div>
     </div>
-  </template>
+</template>
 
 
-  <script>
+<script>
 import { defineComponent } from 'vue';
 import { Carousel, Slide, Navigation } from 'vue3-carousel';
 import 'vue3-carousel/dist/carousel.css';
 import VcSlide from './Slide.vue';
 
 export default defineComponent({
-  name: 'Form',
-  components: {
-    VcSlide,
-    Carousel,
-    Slide,
-    Navigation,
-  },
-  data() {
-    return {
-      list: [],
-    };
-  },
-  methods: {
-    testeFn(slide) {
-      this.list.push(slide);
+    name: 'Form',
+    components: {
+        VcSlide,
+        Carousel,
+        Slide,
+        Navigation,
     },
-  },
+    data() {
+        return {
+            list: [],
+        };
+    },
+    methods: {
+        testeFn(slide) {
+            this.list.push(slide);
+        },
+    },
 });
 </script>
 
@@ -61,36 +61,37 @@ export default defineComponent({
     border: 1px solid #262626;
     border-radius: 4px;
     overflow: hidden;
-    background: linear-gradient(to bottom right, #ffffff, #e0e0e0); /* Fundo em degradê */
+    background: linear-gradient(to bottom right, #ffffff, #e0e0e0);
+    /* Fundo em degradê */
     width: 500px;
     height: 500px;
     margin: 0 auto;
-  }
+}
 
-  .slide-content {
+.slide-content {
     padding: 10px;
     text-align: center;
     height: 100%;
     display: flex;
     flex-direction: column;
     justify-content: center;
-  }
+}
 
-  .slide-content h3 {
+.slide-content h3 {
     font-size: 28px;
     margin-bottom: 10px;
     color: #333;
-  }
+}
 
-  .slide-content div {
+.slide-content div {
     font-size: 18px;
     color: #555;
-  }
+}
 
-  .carousel {
-    max-width: 50vw; /* 50% da largura da tela */
-    margin: 0 auto; /* Centraliza o carrossel */
-  }
-
-
+.carousel {
+    max-width: 50vw;
+    /* 50% da largura da tela */
+    margin: 0 auto;
+    /* Centraliza o carrossel */
+}
 </style>
--- resources/js/Component/Slide.vue
+++ resources/js/Component/Slide.vue
@@ -1,50 +1,51 @@
 <template>
     <div class="row">
-      <div class="col-sm-4">
-        <p>Título</p>
-        <div class="form-group">
-            <input type="text" class="form-control" v-model="title" />
-        </div>
-
-        <p>Descrição</p>
-        <div class="form-group">
-            <textarea type="text" contenteditable="true" name="eventDescription" id="eventDescription" class="form-control" v-model="description" />
-        </div>
-
-        <div class="form-group">
-            <a @click="addSlide({title, description})">Adicionar Slide</a>
-        </div>
-      </div>
-
-      <div class="col-sm-8">
-        <div class="slide">
-            <div class="texto-editavel">
-                {{ title }}
+        <div class="col-sm-4">
+            <p>Título</p>
+            <div class="form-group">
+                <input type="text" class="form-control" v-model="title" />
             </div>
 
-            <div class="texto-editavel" v-html="description">
+            <p>Descrição</p>
+            <div class="form-group">
+                <textarea type="text" contenteditable="true" name="eventDescription" id="eventDescription"
+                    class="form-control" v-model="description" />
+            </div>
 
+            <div class="form-group">
+                <a @click="addSlide({ title, description })">Adicionar Slide</a>
             </div>
         </div>
-      </div>
+
+        <div class="col-sm-8">
+            <div class="slide">
+                <div class="texto-editavel">
+                    {{ title }}
+                </div>
+
+                <div class="texto-editavel" v-html="description">
+
+                </div>
+            </div>
+        </div>
 
     </div>
-  </template>
+</template>
 
-  <script>
+<script>
 import CKEditor from 'ckeditor4-vue';
 
 
-  export default {
+export default {
     name: "slide",
     props: {
         addSlide: Function,
     },
     data() {
-      return {
-        title: 'Seu tiel',
-        description: '<p>Hello from CKEditor 5!</p>',
-      };
+        return {
+            title: 'Seu tiel',
+            description: '<p>Hello from CKEditor 5!</p>',
+        };
     },
 
     methods: {
@@ -52,70 +53,71 @@ import CKEditor from 'ckeditor4-vue';
     },
     mounted() {
         const self = this;
-        var editor = CKEDITOR.replace( 'eventDescription', {
-	        uiColor: '#14B8C4',
-	        toolbar: [
-		        // ['Styles','Format','Font','FontSize'],
-                ['Bold','Italic','Underline','StrikeThrough','-','Undo','Redo','-','Cut','Copy','Paste','Find','Replace','-'],
-                ['NumberedList','BulletedList'],
-	        ]
+        var editor = CKEDITOR.replace('eventDescription', {
+            uiColor: '#14B8C4',
+            toolbar: [
+                // ['Styles','Format','Font','FontSize'],
+                ['Bold', 'Italic', 'Underline', 'StrikeThrough', '-', 'Undo', 'Redo', '-', 'Cut', 'Copy', 'Paste', 'Find', 'Replace', '-'],
+                ['NumberedList', 'BulletedList'],
+            ]
         });
 
-        editor.on( "pluginsLoaded", function( event ){
-            editor.on( 'contentDom', function( evt ) {
+        editor.on("pluginsLoaded", function (event) {
+            editor.on('contentDom', function (evt) {
                 var editable = editor.editable();
-                editable.attachListener( editable, 'keyup', function( e ) {
+                editable.attachListener(editable, 'keyup', function (e) {
                     self.description = e.data.$.target.innerHTML;
                 });
             });
         });
     }
-  };
-  </script>
-  <style scoped>
-  .button {
+};
+</script>
+<style scoped>
+.button {
     margin-top: 35px;
-  }
-  .handle {
+}
+
+.handle {
     float: left;
     padding-top: 8px;
     padding-bottom: 8px;
-  }
+}
 
-  .close {
+.close {
     float: right;
     padding-top: 8px;
     padding-bottom: 8px;
-  }
+}
 
-  input {
+input {
     display: inline-block;
     width: 50%;
-  }
+}
 
-  .text {
+.text {
     margin: 20px;
-  }
+}
 
-  .editor-container {
+.editor-container {
     display: flex;
-  }
+}
 
-  .toolbox {
+.toolbox {
     width: 200px;
     border-right: 1px solid #ccc;
     padding: 10px;
-  }
+}
 
-  .slide-container {
+.slide-container {
     display: flex;
     justify-content: center;
     align-items: center;
     height: 100vh;
     flex-grow: 1;
-  }
+}
 
-  .slide {
+.slide {
     border: 1px solid #ccc;
     padding: 20px;
     text-align: center;
@@ -123,67 +125,71 @@ import CKEditor from 'ckeditor4-vue';
     height: 300px;
     position: relative;
     background-color: #555;
-  }
+}
 
-  .text-container {
+.text-container {
     display: flex;
     flex-direction: column;
     gap: 10px;
-  }
+}
 
-  .texto{
+.texto {
     cursor: grab;
     border: 1px solid #ccc;
     padding: 10px;
     border-radius: 5px;
     background-color: white;
-  }
+}
 
-  .texto-editavel {
-      margin-top: 10px;
-      position: relative;
-      background-color: white;
-    }
+.texto-editavel {
+    margin-top: 10px;
+    position: relative;
+    background-color: white;
+}
 
-    .texto-editavel button {
-      position: absolute;
-      top: 5px;
-      right: 5px;
-      background-color: red;
-      color: white;
-      border: none;
-      border-radius: 50%;
-      width: 20px;
-      height: 20px;
-      font-size: 14px;
-      cursor: pointer;
-      line-height: 16px;
-      text-align: center;
-      padding: 0;
-      display: none; /* Esconde o botão por padrão */
-    }
+.texto-editavel button {
+    position: absolute;
+    top: 5px;
+    right: 5px;
+    background-color: red;
+    color: white;
+    border: none;
+    border-radius: 50%;
+    width: 20px;
+    height: 20px;
+    font-size: 14px;
+    cursor: pointer;
+    line-height: 16px;
+    text-align: center;
+    padding: 0;
+    display: none;
+    /* Esconde o botão por padrão */
+}
 
-    .texto-editavel:hover button {
-      display: block; /* Mostra o botão quando o mouse estiver sobre o texto */
-    }
+.texto-editavel:hover button {
+    display: block;
+    /* Mostra o botão quando o mouse estiver sobre o texto */
+}
 
 
-  .texto[data-type='Title'], .texto-editavel[data-type='Title'] {
+.texto[data-type='Title'],
+.texto-editavel[data-type='Title'] {
     font-size: 24px;
     font-weight: bold;
     color: #333;
-  }
+}
 
-  .texto[data-type='Description'], .texto-editavel[data-type='Description'] {
+.texto[data-type='Description'],
+.texto-editavel[data-type='Description'] {
     font-size: 16px;
     color: #777;
-  }
+}
 
-  .texto[data-type='List'], .texto-editavel[data-type='List'] {
+.texto[data-type='List'],
+.texto-editavel[data-type='List'] {
     font-size: 14px;
     color: #555;
     background-color: #f4f4f4;
     list-style-type: disc;
     padding-left: 20px;
-  }
-  </style>
+}</style>
